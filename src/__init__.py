ddc57b2bef1841a273236a336e24cec11d75a8b4
mrodal <maticosas@gmail.com>
2020-06-11 12:32:10 -0300

    1.3.0 release

--- src/__init__.py
+++ src/__init__.py
@@ -36,7 +36,7 @@ from .objects.message import MessageGroup, factory_message
 from .objects.number_status import NumberStatus
 from .wapi_js_wrapper import WapiJsWrapper
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 
 class WhatsAPIDriverStatus(object):
