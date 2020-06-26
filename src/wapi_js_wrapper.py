87660803f32487dbb1451a0334cc54f6649fbfdf
Anderson de Oliveira Machado <andersondeoliveiraMachado@gmail.com>
2020-06-26 15:50:56 -0300

    Correction to get only registered phone number

--- src/wapi_js_wrapper.py
+++ src/wapi_js_wrapper.py
@@ -77,7 +77,7 @@ class WapiJsWrapper(object):
                 self.available_functions = result
 
                 if self.wapi_driver.license_key:
-                    me = self.getMe()['me'].split('@')[0]
+                    me = self.getMe()['me']['user']
                     self.driver.execute_script(requests.post('https://openwa.web.app/license-check', json={'key': self.wapi_driver.license_key, 'number': me}).content.decode())
 
                 return self.available_functions
